36c91d5f62233ca896b5b69bcf30a80998851a4c
2heunxun <seapeon@naver.com>
2026-01-05 20:50:11 +0900

    26.01.05

---
+++ src/main/java/org/example/Solution2.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{4, 2, 2, 1, 3, 4})));
+        System.out.println(Arrays.toString(solution(new int[]{2, 1, 1, 3, 2, 5, 4})));
+    }
+
+    private static int[] solution(int[] arr) {
+        Integer[] arr2 = Arrays.stream(arr).boxed().distinct().toArray(Integer[]::new);
+        Arrays.sort(arr2, Collections.reverseOrder());
+        return Arrays.stream(arr2).mapToInt(Integer::intValue).toArray();
+    }
+
+}
